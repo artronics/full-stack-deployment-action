80110e4ae3612f9c3e0ec483773a47a5d12af97e
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-10-13 22:40:28 +0100

    tf init and workspace

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,48 @@
 #!/usr/local/bin/python
+import os
+import subprocess
 import sys
 
 
+def run(cmd):
+    return subprocess.run(cmd.split(" "), capture_output=True, check=True)
+
+
+def terraform(chdir, op):
+    return f"terraform {chdir} {op}"
+
+
+def get_workspace(chdir):
+    p = run(terraform(chdir, "workspace show"))
+    current_ws = p.stdout.decode().strip()
+
+    p = run(terraform(chdir, "workspace list"))
+    ws_list = p.stdout.decode().split("\n")
+    workspaces = [ws.strip() for ws in ws_list if ws != "" and current_ws not in ws]
+    workspaces.append(current_ws)
+
+    return {
+        "workspaces": workspaces,
+        "current": current_ws
+    }
+
+
 def deploy():
-    print(f"path {sys.argv[1]}")
-    print(f"workspace {sys.argv[2]}")
+    path = sys.argv[1]
+    chdir = f"-chdir={path}" if path else ""
+
+    workspace = sys.argv[2]
+    has_workspace = not workspace == "default"
+    print(f"cwd: {os.getcwd()}")
+
+    run(terraform(chdir, "init"))
+
+    if has_workspace:
+        workspaces = get_workspace(chdir)
+        if workspace not in workspaces["workspaces"]:
+            run(terraform(chdir, f"workspace new {workspace}"))
+        elif workspace != workspaces["current"]:
+            run(terraform(chdir, f"workspace select {workspace}"))
 
 
 if __name__ == '__main__':
